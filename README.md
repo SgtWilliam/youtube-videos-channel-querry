a790a8ad23fca8c4b0ff71429411f222a0f08466
Sgt_William <williampaes097@gmail.com>
2023-01-16 08:40:05 -0300

    V2

---
+++ README.md
@@ -0,0 +1,8 @@
+# How to use
+### 1) Create youtube api Key, [Tutorial](https://www.youtube.com/watch?v=N18czV5tj5o)
+### 2) Put your API key in youtube.setKey('') in the main file
+### 3) Take the channel id and put it in channelId: "" in the main file
+
+
+
+### Script created by [Sgt_William](https://github.com/SgtWilliam) <3
--- main.js
+++ main.js
@@ -1,14 +1,81 @@
-const YoutubeApiConnect = require("./Repository/YoutubeApiConnect");
+const YouTube = require('youtube-node');
+const youtube = new YouTube();
 
-YoutubeApiConnect.queryChannelVideos().then(r => {
-    // console.log(r.items[0]["id"]["videoId"])
+// Put API key here
+youtube.setKey('Api key here');
 
-    for(let i = 0; i <= 49; i++) {
-        try {
-            console.log(r.items[i]["id"]["videoId"])
-        } catch (e) {
-            console.log(`erro no loop ${i}`)
+let nextPageToken = '';
+
+const YoutubeApiConnect = {
+
+    async queryChannelVideos(nextPageToken){
+        return new Promise((resolve, reject) => {
+            youtube.search(
+                '',
+                50,
+                {
+                    type: 'video',
+                    pageToken: nextPageToken,
+// Put channelId
+                    channelId: "Channel ID HERE"
+                },
+                (error, result) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(result);
+                        for(let i = 0; i <= 49; i++) {
+                            try {
+                                YoutubeApiConnect.searchVideosViewers(result.items[i].id.videoId)
+                                //console.log( result.items[i].snippet.title);
+                            } catch (e) {
+                                console.log(`erro on loop ${i}`)
+                            }
+                        }
+                    }
+                }
+            );
+        });
+    },
+
+    async searchVideosViewers(videoId){
+        youtube.getById(videoId,  function (error, result) {
+            if (error) {
+                console.log(error);
+            } else {
+                // const YouViewers = result.items[0].statistics.viewCount
+                async function consoleFunction(){
+                    await console.log(`https://www.youtube.com/watch?v=${videoId} have ${result.items[0]["statistics"].viewCount} Views`)
+                }
+                if(result.items[0]["statistics"].viewCount > 40000){
+                    consoleFunction()
+                };
+            }
+        })
+    }
+};
+
+
+async function createLoop() {
+
+    while (nextPageToken !== null) {
+        const result = await YoutubeApiConnect.queryChannelVideos(nextPageToken);
+        if(result.nextPageToken == null){
+            break
+        } else {
+            nextPageToken = result.nextPageToken;
+            //console.log(`https://www.youtube.com/watch?v=${result.items[0].id.videoId}`)
         }
+        //console.log(nextPageToken)
     }
-    }
-);
+}
+//console.log(nextPageToken)
+createLoop();
+
+
+module.exports = YoutubeApiConnect;
+
+
+// BY Sgt_William <3
+
+
